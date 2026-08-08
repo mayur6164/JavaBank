9915b0909839173048f518b38da43a50b7ec7199
Mayur Kariya <mayurdevrajkariya@gmail.com>
2026-08-08 15:55:47 +0530

    Make program using class and object and constructor where parameterized constructor was used to assign value of new customer of bank and displaying data of customer.

--- bank.java
+++ bank.java
@@ -1,7 +1,27 @@
 import java.util.*;
 
+class JavaBank{
+    String name;
+    int accNo, bal;
+
+    public JavaBank(String name, int accNo, int bal){
+        this.name = name;
+        this.accNo = accNo;
+        this.bal = bal;
+    }
+
+    public void display(){
+        System.out.println("Name: " + name);
+        System.out.println("Account Number: " + accNo);
+        System.out.println("Balance: " + bal);
+    }
+}
+
 public class bank{
     public static void main(String[] args){
-        System.out.println("Hi, This is Bank Program.");
+        JavaBank c1 = new JavaBank("Mayur Kariya",101,15000);
+        JavaBank c2 = new JavaBank("Vishnu Kariya",102,10000);
+        c1.display();
+        c2.display();
     }
 }
